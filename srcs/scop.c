6ab038ffcc9f031d19b3c0e74ab18cb7027e8f60
Jean <jflorimo@student.42.fr>
2015-06-11 12:32:37 +0200

    two model and rotation

--- srcs/scop.c
+++ srcs/scop.c
@@ -50,18 +50,107 @@ int main(void)
 		1.0f, -1.0f, 0.0f,
 		0.0f,  1.0f, 0.0f,
 	};
+	static const GLfloat color_buffer_data[] = {
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+	};
 
-	// This will identify our vertex buffer
-	GLuint vertex_buffer;
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, // triangle 1 : end
+		1.0f, 1.0f,-1.0f, // triangle 2 : begin
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f, // triangle 2 : end
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f
+	};
 
-	// Generate 1 buffer, put the resulting identifier in vertex_buffer
-	glGenBuffers(1, &vertex_buffer);
+	static const GLfloat g_color_buffer_data[] = {
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
+	};
 
-	// The following commands will talk about our 'vertex_buffer' buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	GLuint vertex_buffer[2];
+	glGenBuffers(1, &vertex_buffer[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	// Give our vertices to OpenGL.
+	glGenBuffers(1, &vertex_buffer[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer[1]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+	GLuint colorbuffer[2];
+	glGenBuffers(1, &colorbuffer[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &colorbuffer[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
 //-----
 	GLuint programID = LoadShaders("shaders/vertexShader", "shaders/fragmentShader");
 
@@ -69,14 +158,20 @@ int main(void)
 	GLuint VID = glGetUniformLocation(programID, "V");
 	GLuint PID = glGetUniformLocation(programID, "P");
 
-	t_matrix M =  init_matrix_translation(0, 0, 0);
+	t_matrix M[2];
+	M[0] =  init_matrix_translation(0, 0, 0);
+	M[1] =  init_matrix_translation(5, 1, 0);
 	t_matrix V =  init_matrix_translation(0, 0, 0);
 	t_matrix P =  init_perspective(60 * M_PI / 180, 640/480, 0.1, 100);
 
-	t_matrix trans = init_matrix_translation(5, 0, 10);
-	t_matrix rotate = init_matrix_rotation_y(0.1);
+	t_matrix trans = init_matrix_translation(0, -3, 10);
+	t_matrix rotate = init_matrix_rotation_y(0.01);
+	t_matrix rotate2 = init_matrix_rotation_y(-0.01);
 
-	V = multiply(trans, V);
+	V = multiply(V, trans);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -86,30 +181,50 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram(programID);
 
-		glUniformMatrix4fv(MID, 1, GL_FALSE, &M.m[0][0]);
+
 		glUniformMatrix4fv(VID, 1, GL_FALSE, &V.m[0][0]);
 		glUniformMatrix4fv(PID, 1, GL_FALSE, &P.m[0][0]);
 
-		V = multiply(rotate, V);
+	
+		M[0] = multiply(rotate,M[0]);
+		M[1] = multiply(rotate2,M[1] );
+		int i = 0;
+		int sizes[] = {12, 1};
+		while (i < 2)
+		{
 
-//-----
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-		glVertexAttribPointer(
-			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer of-lfset
-		);
+			glUniformMatrix4fv(MID, 1, GL_FALSE, &M[i].m[0][0]);
+			// 1rst attribute buffer : vertices
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer[i]);
+			glVertexAttribPointer(
+				0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+				3,                  // size
+				GL_FLOAT,           // type
+				GL_FALSE,           // normalized?
+				0,                  // stride
+				(void*)0            // array buffer of-lfset
+			);
+
+			glEnableVertexAttribArray(1);
+			glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[i]);
+			glVertexAttribPointer(
+			    1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+			    3,                                // size
+			    GL_FLOAT,                         // type
+			    GL_FALSE,                         // normalized?
+			    0,                                // stride
+			    (void*)0                          // array buffer offset
+			);
+
+			glDrawArrays(GL_TRIANGLES, 0, sizes[i]*3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+			glDisableVertexAttribArray(0);
+			i++;
+		}
+
 
 
 		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-
-		glDisableVertexAttribArray(0);
 
 //-----
 
